4163d5a2e8bdec9022c4aab65558df2404aa1eb7
Sam Perkins <perkins.samd12@gmail.com>
2024-12-05 10:39:41 -0500

    feat(aoc_4_2): Att 2; 2D array to check for diagonal pair, remove cardinal checks

--- js/4-2.js
+++ js/4-2.js
@@ -9,72 +9,63 @@ very similar names
 cardinal "MAS" would be just as valid as two cardinal or two diagonal "MAS" but this does not seem correct based on the
 sample data so I changed the `moves` to be a 3D array that contains an array of cardinal index arrays and an array of
 diagonal index arrays. Previously it was a 2D array.
+--ATTEMPT 2--
+* Cardinal directions do not count, only diagonal directions. I did not think the "X" aspect was quite so literal. Of
+course the first attempt still worked with the sample data because the sample data did not have any matches at cardinal
+directions in the first place
  */
 
-const {sample, data} = require('./4-1')
+const {sample, data: startData} = require('./4-1')
+
+const data = startData
 
 function doChallenge () {
   let total = 0
 
   for (let i = 0; i < data.length; i++) {
     const row = data[i]
-    for (let j = 0; j < row.length; j++) {
+    charloop: for (let j = 0; j < row.length; j++) {
       const char = row[j]
 
       // To simplify the logic, we will only check for matches when we can
       // use "A" as our starting point
       if (char !== 'A') continue
 
-      // Matches will take place using one of the cardinal/diagonal directions and its inverse
-      // For it to be a match it either both need to be cardinal or both need to be diagonal
+      // Matches will take place using the character in any diagonal direction and the character at coordinate inverse
       const moves = [
-        [
-          [-1, 0],
-          [0, 1]
-        ], [
-          [-1, 1],
-          [1, 1]
-        ]
+        [-1, 1],
+        [1, 1]
       ]
 
-      movesloop: for (const move of moves) {
-        let count = 0
+      // Since the "MAS" needs to be paired by diagonal, if one of the pairs fails
+      // we can proceed to the next set of pairs
+      for (const [iIt, jIt] of moves) {
+        // One char from the pattern should appear at the specified index and the other at its inverse
+        const pattern = ['M', 'S']
+        let index = i + iIt
+        let jndex = j + jIt
+        const aMatch = pattern.indexOf(data[index]?.[jndex])
+        let bMatch
 
-        // Since the "MAS" needs to be paired by cardinal/diagonal, if one of the pairs fails
-        // we can proceed to the next set of pairs
-        for (const [iIt, jIt] of move) {
-          // One char from the pattern should appear at the specified index and the other at its inverse
-          const pattern = ['M', 'S']
-          let index = i + iIt
-          let jndex = j + jIt
-          const aMatch = pattern.indexOf(data[index]?.[jndex])
-          let bMatch
+        if (aMatch === -1) continue charloop
 
-          if (aMatch === -1) {
-            continue movesloop
-          }
+        // I think reassigning a value is faster than deleting it?
+        pattern[aMatch] = null
+        index = i + (iIt * -1)
+        jndex = j + (jIt * -1)
+        bMatch = pattern.indexOf(data[index]?.[jndex])
 
-          // I think reassigning a value is faster than deleting it?
-          pattern[aMatch] = null
-          index = i + (iIt * -1)
-          jndex = j + (jIt * -1)
-          bMatch = pattern.indexOf(data[index]?.[jndex])
-
-          if (bMatch === -1) {
-            continue movesloop
-          }
-
-          // "M" is on one side of the "A" and "S" is on the other
-          count++
-        }
-
-        // VALID: "MAS" was found in both cardinal or both diagonal directions
-        if (count === 2) total++
+        if (bMatch === -1) continue charloop
       }
+
+      // VALID: "MAS" was found in both diagonal directions or the
+      // `charloop` would have continued to the next character
+      total++
     }
   }
 
-  // 4-2 Answer: 2016
+  // 2016 was too high
+  // 4-2 Answer: 1998
   console.log(total)
 }
 
