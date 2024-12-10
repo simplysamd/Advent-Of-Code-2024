6191e5211808af9af11ec5e90b5e8b6b74874ff6
Sam Perkins <perkins.samd12@gmail.com>
2024-12-09 22:07:21 -0500

    feat(aoc_8_1): Att 1; record positions of each character and calc nodes from those positions

---
+++ js/8-1.js
@@ -0,0 +1,75 @@
+/*
+--ATTEMPT 1--
+* Opting to iterate through each character in the grid and record each location in a group with the other locations for
+that character
+* After everything is grouped we can move through each character and iterate over each location, first identifying the
+nodes it creates with every other location of that type and then removing it from the array and repeating the process
+the next location
+* At the end we turn them into unique strings and throw them in a Set to make sure there's no dupes
+ */
+
+const fs = require('fs')
+
+function findAntinodes (locations, ranges) {
+  let antinodes = []
+  const focus = locations[0]
+
+  for (let i = 1; i < locations.length; i++) {
+    const location = locations[i]
+    // Offset between points can be used to determine the location of corresponding nodes
+    const diff = [focus[0] - location[0], focus[1] - location[1]]
+    const aloc = [focus[0] +  diff[0], focus[1] + diff[1]]
+    const bloc = [location[0] + (diff[0] * -1), location[1] + (diff[1] * -1)]
+
+    // Do not push any locations that are outside the bounds
+    if (aloc[0] >= ranges.v[0] && aloc[1] <= ranges.v[1]) antinodes.push(aloc)
+    if (bloc[0] >= ranges.h[0] && bloc[1] <= ranges.h[1]) antinodes.push(bloc)
+  }
+
+  // After finding all the relationships between the current location and others
+  // we can safely remove the current location and move on to the next
+  if (locations.length > 1) antinodes.push(...findAntinodes(locations.slice(1), ranges))
+
+  return antinodes
+}
+
+function doChallenge () {
+  const uniqueAntinodes = new Set()
+  const dataFile = fs.readFileSync('../data/data_aoc_8.txt', 'utf8')
+  const data = dataFile.replaceAll('\r', '').split('\n')
+  const ranges = {v: [0, data.length - 1], h: [0, data[0].length - 1]}
+  const map = {}
+
+  for (const each of data) {
+    console.log(each)
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      const value = data[i][j]
+      // Instead of looking for individual characters we'll look for "not" character
+      // since all "nots" are represented with a singular character
+      if (value === '.') continue
+      if (!map[value]) map[value] = []
+
+      // Record and group all locations of each unique character
+      map[value].push([i, j])
+    }
+  }
+
+  // const antinodes = findAntinodes(map['0'], ranges)
+  // console.log('nodes', antinodes)
+
+  for (const character in map) {
+    const antinodes = findAntinodes(map[character], ranges)
+    for (const each of antinodes) {
+      uniqueAntinodes.add(each.join('-'))
+    }
+  }
+
+  // 8-1 Answer: 497
+  console.log(uniqueAntinodes.size)
+}
+
+// Run 8-1
+doChallenge()
