d88b643023c2517b7d925eebf6ff0d285bff4c67
Sam Perkins <perkins.samd12@gmail.com>
2024-12-10 17:52:17 -0500

    feat(aoc_9_1): Att 2; forget .pop, just loop like a normal person

--- js/9-1.js
+++ js/9-1.js
@@ -1,11 +1,14 @@
 /*
---Attempt 1--
+--ATTEMPT 1--
 * I think we managed this one okay? Break out the data into values and empty spaces, fill the empty spaces from the end
 of the array by popping them off, keep going till there's nothing left to pop.
+--ATTEMPT 2--
+* We did not manage this one okay :(
+* I updated my logic to not do the `.pop` strategy and tested some edge cases with `0`
 */
 const fs = require('fs')
 
-const data = fs.readFileSync('../data/data_aoc_9.txt', 'utf8')
+const data = fs.readFileSync('../data/sample_aoc_9.txt', 'utf8')
 
 function doChallenge () {
   let total = 0
@@ -14,6 +17,9 @@ function doChallenge () {
   for (let i = 0; i < data.length; i++) {
     let even = i % 2 === 0
     for (let j = 0; j < Number(data[i]); j++) {
+      if (Number(data[i]) === 0) {
+        console.log('its 0')
+      }
       if (even) {
         filesystem.push(i / 2)
       } else {
@@ -22,25 +28,35 @@ function doChallenge () {
     }
   }
 
-  for (let i = 0; i < filesystem.length; i++) {
-    // Fill in the `null`s with data from the end of the array
+  console.log(filesystem)
+
+  filesystemloop: for (let i = 0; i < filesystem.length; i++) {
     if (filesystem[i] === null) {
       let x
-      // `x` needs to be a non-null value. This `while` loop does NOT seem safe,
-      // I should probably consider an alternate approach so I don't pop the whole array
-      while (x === null || x === undefined) x = filesystem.pop()
+      for (let j = filesystem.length - 1; j >= 0; j--) {
+        // Will I ever need this condition?
+        if (j === i) {
+          break filesystemloop
+        }
 
-      // The value we get back from this process is the value we need to update the
-      // "filesystem checksum" so we do not actually need to update the array - but
-      // we will anyway just for fun
-      filesystem[i] = x
-      total += (i * x)
-    } else {
-      total += (i * filesystem[i])
+        if (filesystem[j] !== null && filesystem[j] >= 0) {
+          x = filesystem[j]
+          filesystem[i] = x
+          filesystem[j] = null
+          continue filesystemloop
+        }
+      }
     }
   }
 
-  // 9-1 Answer: 6260050832569
+  console.log(filesystem)
+
+  for (let i = 0; i < filesystem.length; i++) {
+    total += (i * filesystem[i])
+  }
+
+  // 6260050832569 was too high
+  // 9-1 Answer: 6259790630969
   console.log(total)
 }
 
