738bd59554acc7558139b1a1f362aee84a26062a
Sam Perkins <perkins.samd12@gmail.com>
2024-12-10 22:14:32 -0500

    refactor(aoc_9_2): Att 2; change splice operations to assignment

--- js/9-2.js
+++ js/9-2.js
@@ -8,6 +8,10 @@ to move each file block one time. Iterating over the gaps at the start of the ar
 and left to remain so it can potentially be filled by another file block. Also, we need to continue replacing the file
 block that gets moved with the blank space - they are TRADING places, the empty space does not disappear. Empty spaces
 will still increment the index for the final calculations
+--ATTEMPT 2--
+* First attempt was too high
+* Changed the logic to do direct assignment instead of splicing because splicing was doing some weird things and I was
+having a hard time identifying the issue. Direct assignment should be fine I think?
 */
 const fs = require('fs')
 
@@ -20,31 +24,31 @@ function doChallenge () {
   for (let i = 0; i < data.length; i++) {
     let even = i % 2 === 0
 
-    if (Number(data[i]) === 0) continue
-
     filesystem.push(new Array(Number(data[i])).fill(even ? i / 2 : null))
   }
 
   for (let i = filesystem.length - 1; i > 0; i--) {
     if (filesystem[i][0] === null) continue
+    const fileBlock = filesystem[i].slice()
 
     for (let j = 0; j < i; j++) {
       if (filesystem[j][0] !== null) continue
 
-      const len = filesystem[i].length
-      const gap = filesystem[j].length
+      const gapBlock = filesystem[j].slice()
+      const len = fileBlock.length
+      const gap = gapBlock.length
       const diff = gap - len
 
       if (diff < 0) continue
-      if (diff === 0) {
-        filesystem.splice(j, 1, filesystem[i])
-        filesystem.splice(i, 1, new Array(gap).fill(null))
-        continue
-      }
 
       const remainder = new Array(diff).fill(null)
-      filesystem.splice(j, 1, filesystem[i], remainder)
-      filesystem.splice(i + 1, 1, new Array(len).fill(null))
+      filesystem[j] = fileBlock
+      filesystem[i] = new Array(len).fill(null)
+
+      if (remainder.length) {
+        filesystem.splice(j + 1, 0, remainder)
+      }
+
       break
     }
   }
@@ -55,7 +59,8 @@ function doChallenge () {
     total += (i * flatfiles[i])
   }
 
-  // 9-2 Answer: 7458698291531
+  // 7458698291531 was too high
+  // 9-2 Answer: 6352043857526
   console.log(total)
 }
 
